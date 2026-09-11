567b09bddccb2a364c51bcd046683828cb450045
EdwinBrook <76277302+EdwinBrook@users.noreply.github.com>
2026-09-11 19:36:33 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.snakegame', // 保留你原来的 appId 和 appName
-  appName: 'snake-game',
-  webDir: 'dist'
-};
-
-export default config;
